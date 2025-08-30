a5a25b622a9ce54d63df8079eabdb4ba956173bb
ianer-glitch <ianrepker@gmail.com>
2025-08-30 06:19:28 -0300

    feat: added uow to CreateUserUseCase.cs

--- PraOnde.API/Application/UseCases/User/CreateUser/CreateUserUseCase.cs
+++ PraOnde.API/Application/UseCases/User/CreateUser/CreateUserUseCase.cs
@@ -2,18 +2,20 @@ using FluentValidation;
 using PraOnde.API.Application.Common;
 using PraOnde.API.Domain.Exceptions;
 using PraOnde.API.Infraestructure.Data.Repositories;
+using PraOnde.API.Infraestructure.Data.UnitOfWork;
 
 namespace PraOnde.API.Application.UseCases.User.CreateUser;
 
 public class CreateUserUseCase : ICreateUserUseCase
 {
     private readonly ILogger<CreateUserUseCase> _logger;
-    private readonly IRepository<Domain.Entities.User> _userRepository; 
+    private readonly IUnitOfWork _uow;
     
-    public CreateUserUseCase(ILogger<CreateUserUseCase> logger, IRepository<Domain.Entities.User> userRepository)
+    public CreateUserUseCase(ILogger<CreateUserUseCase> logger, IUnitOfWork uow)
     {
         _logger = logger;
-        _userRepository = userRepository;
+        _uow = uow;
+        
     }
 
     public async Task<Result<CreateUserUseCaseOut>> ExecuteAsync(CreateUserUseCaseIn req)
@@ -25,7 +27,7 @@ public class CreateUserUseCase : ICreateUserUseCase
             var validator = new CreateUserUseCaseValidator();
             await validator.ValidateAndThrowAsync(req);
 
-            var existingUser = await _userRepository.FirstOrDefaultAsync(f => f.Name.Equals(req.Username));
+            var existingUser = await _uow.UserRepository.FirstOrDefaultAsync(f => f.Name.Equals(req.Username));
             if (existingUser != null)
             {
                 _logger.LogInformation(
@@ -35,9 +37,11 @@ public class CreateUserUseCase : ICreateUserUseCase
 
             var newUser = new Domain.Entities.User(req.Username);
 
-            await _userRepository.AddAsync(newUser);
+            await _uow.BeginTransactionAsync();
+            
+            await _uow.UserRepository.AddAsync(newUser);
 
-            if (await _userRepository.SaveChangesAsync() > 0)
+            if (await _uow.CommitAsync() > 0)
             {
                 _logger.LogInformation($"[CreateUserUseCase] Created user {req.Username}");
                 return Result<CreateUserUseCaseOut>.Success(new CreateUserUseCaseOut()
@@ -50,16 +54,19 @@ public class CreateUserUseCase : ICreateUserUseCase
         }
         catch (UserAlreadyExistException ex)
         {
+            await _uow.RollbackAsync();
             _logger.LogError($"[CreateUserUseCase] {ex.Message},  stack trace: {ex.StackTrace}");
             return Result<CreateUserUseCaseOut>.Fail($"Já existe um usuário cadastrado com o nome {req.Username}");
         }
         catch (ValidationException ex)
         {
+            await _uow.RollbackAsync();
             _logger.LogError($"[CreateUserUseCase] {ex.Message},  stack trace: {ex.StackTrace}");
             return Result<CreateUserUseCaseOut>.Fail(ex.Message);
         }
         catch (Exception ex)
         {
+            await _uow.RollbackAsync();
             _logger.LogError(
                 $"[CreateUserUseCase] Ocurred an error while attempting to create a new user: {ex.Message},  stack trace: {ex.StackTrace}");
             return Result<CreateUserUseCaseOut>.Fail("Ocorreu um erro desconhecido e não foi possível criar o usuário");
